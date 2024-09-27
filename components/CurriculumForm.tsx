76e59ad70477e2dc63ba57b6e941cf3c61818d23
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-09-27 20:59:48 +0530

    wagmi: bug fixes & improvements

--- components/CurriculumForm.tsx
+++ components/CurriculumForm.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
+"use client";
+
+import { useChat } from "ai/react";
+import React from "react";
 
 const CurriculumForm = () => {
+  const { messages, handleInputChange, handleSubmit, input } = useChat();
 
-  const level = 'beginner'
-  const topic = 'software engineering'
-  const duration = '4 months'
+  const level = "beginner";
+  const topic = "software engineering";
+  const duration = "4 months";
 
   const prompt = `
     Generate a comprehensive learning roadmap for a ${level} student learning ${topic}. 
@@ -14,9 +18,19 @@ const CurriculumForm = () => {
 
   return (
     <div>
-      Curriculum Form...
+      <form onSubmit={handleSubmit}>
+        <div className="fixed bottom-0 left-1/2 m -translate-x-1/2 transfor w-full py-8 bg-background flex items-center justify-center gap-3">
+          <input
+            className="dark:bg-transparent/30 w-[70%] sm:w-1/2 sm:p-3 p-2 border-2 rounded-xl shadow-md text-black dark:text-white "
+            value={input}
+            placeholder="Say something..."
+            onChange={handleInputChange}
+          />
+          <button>send</button>
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
 export default CurriculumForm;
