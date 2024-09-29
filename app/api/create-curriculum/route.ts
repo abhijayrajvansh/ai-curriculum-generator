753d8c8517b76a2000eee1c57dec401bd404ac33
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-09-29 17:09:03 +0530

    update: streaming ai reponse

--- app/api/create-curriculum/route.ts
+++ app/api/create-curriculum/route.ts
@@ -1,13 +1,23 @@
 import { createOllama } from "ollama-ai-provider";
+import { streamText } from "ai";
+import { z } from 'zod';
 
 const ollama = createOllama({
-  baseURL: process.env.OLLAMA_BASE_URL as string
-})
+  baseURL: process.env.OLLAMA_BASE_URL as string,
+});
 
 export async function POST(req: Request) {
   const { prompt } = await req.json();
 
-  return Response.json({
-    prompt
+  const { text } = await streamText ({
+    model: ollama("llama3.2:3b"), 
+    prompt: prompt,
+    // output: 'no-schema'
   })
+
+  console.log({text})
+
+  return Response.json({
+    msg: "success"
+  });
 }
